0c0c976439683435df920bb137ac50118a1a8b8c
deypadma2020 <deypadma2020@gmail.com>
2025-08-20 00:11:08 +0530

    remove print statements

--- app/db/connection.py
+++ app/db/connection.py
@@ -5,6 +5,4 @@ from pymongo import MongoClient
 load_dotenv(".env")
 
 mongo_url = os.getenv("mongo_url")
-print(mongo_url)
 client = MongoClient(mongo_url)
-print("Python-MongoDB connection created succesfully -\n", client)
